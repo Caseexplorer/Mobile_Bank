bb70326b81f48eaba6369bd575a6c1f9ff42307b
Akinator <zjralwh@163.com>
2021-12-11 12:04:17 +0800

    fix #12.11 12:00

---
+++ app/src/main/java/com/example/mobilebank/ui/dashboard/Bankcard.java
@@ -0,0 +1,16 @@
+package com.example.mobilebank.ui.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobilebank.R;
+
+public class Bankcard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bankcard);
+    }
+}
--- app/src/main/java/com/example/mobilebank/ui/dashboard/Cardfee.java
+++ app/src/main/java/com/example/mobilebank/ui/dashboard/Cardfee.java
@@ -2,16 +2,31 @@ package com.example.mobilebank.ui.dashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mobilebank.R;
 
 public class Cardfee extends AppCompatActivity {
+
+    private Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardfee);
         setTitle("校园卡缴费");
+
+        next = findViewById(R.id.Cardnext);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Cardfee.this,Cardinfo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
