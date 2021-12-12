ba264b76e7076b0d5ec22c76d52bf6f8086a6393
Akinator <zjralwh@163.com>
2021-12-12 17:50:09 +0800

    add bankcard choose

--- app/src/main/java/com/example/mobilebank/ui/dashboard/Bankcard.java
+++ app/src/main/java/com/example/mobilebank/ui/dashboard/Bankcard.java
@@ -2,15 +2,57 @@ package com.example.mobilebank.ui.dashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.example.mobilebank.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Bankcard extends AppCompatActivity {
+
+    private ListView listView;
+    private SimpleAdapter adapter;
+    private List<Map<String,Object>>list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bankcard);
+        setTitle("选择账户");
+
+        listView = findViewById(R.id.mainListView2);
+        list = new ArrayList<>();
+        adapter = new SimpleAdapter(this,a(),R.layout.item,new String[]{"图片","文字"},new int []{R.id.itemImageView1,R.id.itemTextView1});
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i == 0)
+                {
+                    Intent intent = new Intent(Bankcard.this,Cardfee.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+    }
+
+    private List<Map<String,Object>>a()
+    {
+        Map<String,Object>map1 = new HashMap<String,Object>();
+        map1.put("图片",R.drawable.bankcard);
+        map1.put("文字","长城电子借记卡");
+        list.add(map1);
+        return list;
     }
 }
--- app/src/main/java/com/example/mobilebank/ui/dashboard/Cardfee.java
+++ app/src/main/java/com/example/mobilebank/ui/dashboard/Cardfee.java
@@ -12,6 +12,7 @@ import com.example.mobilebank.R;
 public class Cardfee extends AppCompatActivity {
 
     private Button next;
+    private Button choosecard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +21,20 @@ public class Cardfee extends AppCompatActivity {
         setTitle("校园卡缴费");
 
         next = findViewById(R.id.Cardnext);
+        choosecard = findViewById(R.id.Cardchoose);
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Cardfee.this,Cardinfo.class);
+                startActivity(intent);
+            }
+        });
+
+        choosecard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Cardfee.this,Bankcard.class);
                 startActivity(intent);
             }
         });
